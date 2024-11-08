22481c08d3eb4a1e70fb5ae848def4d3aa89035b
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2024-11-08 14:51:40 +0000

    Added QuestionsController for managing quiz questions
    - Implemented `new` and `create` actions to add questions to a quiz.
    - Added `set_quiz` to ensure each question is linked to the correct quiz.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,26 @@
 class QuestionsController < ApplicationController
-end
+    before_action :set_quiz
+  
+    def new
+      @question = @quiz.questions.build
+    end
+  
+    def create
+      @question = @quiz.questions.build(question_params)
+      if @question.save
+        redirect_to quiz_path(@quiz)
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def set_quiz
+      @quiz = Quiz.find(params[:quiz_id])
+    end
+  
+    def question_params
+      params.require(:question).permit(:content, :correct_answer)
+    end
+  end
