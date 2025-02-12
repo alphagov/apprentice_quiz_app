12143875af085c2b5bb1b138f021176d888d9bc2
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2025-02-12 13:51:08 +0000

    Updated Gemfile and asset pipeline for GOV.UK components and DartSass integration
    - Moved govuk_publishing_components and dartsass-rails from their original positions to be grouped with the other production gems to make the Gemfile layout more consistent and ensure that they're available in all environments.
    - Removed the import statements for Turbo and controllers from app/assets/javascript/application.js, which solves the errors caused by the old importmap approach.
    - Removed the require for govuk_publishing_components/components/back-link from app/assets/javascript/application.js since that file doesn't exist (the back-link component doesn't include JavaScript).
    - Resolved issues with DartSass configuration by following the troubleshooting tips from dartsass-rails. Installed the foreman gem so that ./bin/dev runs properly, ensuring DartSass is active and the component guide is accessible locally.

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,7 +1,3 @@
-import "@hotwired/turbo-rails"
-import "controllers"
-
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
-//= require govuk_publishing_components/components/back-link
 //= require govuk_publishing_components/components/button
