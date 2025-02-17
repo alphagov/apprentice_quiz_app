ae14e9da47edc0ead06b661684b9cff93027c9d1
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2025-02-17 13:26:14 +0000

    Added edit, update, and destroy actions for the questions specifically
    - Added before_action :find_question for edit, update, and destroy.
    - Implemented edit, update, and destroy actions to manage the questions for each quiz
    - Added notices for successfully performing a create, update, or delete action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_quiz
+  before_action :find_question, only: [:edit, :update, :destroy]
 
   def new
     @question = @quiz.questions.build
@@ -8,16 +9,37 @@ class QuestionsController < ApplicationController
   def create
     @question = @quiz.questions.build(question_params)
     if @question.save
-      redirect_to quiz_path(@quiz)
+      redirect_to quiz_path(@quiz), notice: "Question was successfully created."
     else
       render :new
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to quiz_path(@quiz), notice: "Question was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to quiz_path(@quiz), notice: "Question was successfully deleted."
   end
 
 private
 
   def find_quiz
     @quiz = Quiz.find(params[:quiz_id])
+  end
+
+  def find_question
+    @question = @quiz.questions.find(params[:id])
   end
 
   def question_params
