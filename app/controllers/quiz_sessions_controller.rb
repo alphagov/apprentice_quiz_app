4d55b68a9b3535451cf5f5dc970fcf139a3beab5
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2025-02-18 10:08:49 +0000

    Ran bundle exec rubocop -a to correct offenses - Auto-corrected 5 offenses with RuboCop

--- app/controllers/quiz_sessions_controller.rb
+++ app/controllers/quiz_sessions_controller.rb
@@ -1,11 +1,10 @@
 class QuizSessionsController < ApplicationController
-    def new
-      @quiz = Quiz.find(params[:quiz_id])
-    end
-  
-    def create
-      @quiz = Quiz.find(params[:quiz_id])
-      redirect_to quiz_path(@quiz), notice: "Quiz submitted! (Result evaluation pending)"
-    end
+  def new
+    @quiz = Quiz.find(params[:quiz_id])
   end
-  
+
+  def create
+    @quiz = Quiz.find(params[:quiz_id])
+    redirect_to quiz_path(@quiz), notice: "Quiz submitted! (Result evaluation pending)"
+  end
+end
