722584f3cbcc5450aee4d62bb1bd4189117c1f1e
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2025-02-21 16:05:40 +0000

    Added spec for the POST create action
    - Implemented a test to verify that a new question is created and that the user is redirected to the quiz show page, with the question content appearing on the page that they're redirected to

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe "Questions", type: :request do
       expect(response.body).to include("Add a New Question")
     end
   end
+
+  describe "POST create" do
+    it "creates a new question and redirects to the quiz show page" do
+      expect {
+        post quiz_questions_path(quiz), params: { question: { content: "One", correct_answer: "Two" } }
+      }.to change(quiz.questions, :count).by(1)
+
+      quiz.questions.last
+      expect(response).to redirect_to(quiz_path(quiz))
+      follow_redirect!
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include("One")
+    end
+  end
 end
