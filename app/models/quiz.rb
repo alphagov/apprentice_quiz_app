b6f0ae2fafaad7200744b815e55e3be8f35f4129
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2025-02-25 13:38:03 +0000

    Added explicit ordering for questions by created_at
    - Updated the Quiz model to always return questions ordered by :created_at
    - For a future improvement, I'll try adding a dedicated 'position' column for user ordering

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,5 @@
 class Quiz < ApplicationRecord
-  has_many :questions, dependent: :destroy
+  has_many :questions, -> { order(:created_at) }, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
 end
