3a1e1d65296d190fd1e42ea049d83d8cb8063692
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2025-03-04 10:29:26 +0000

    Implemented enum for correct_option in Question model
    - Modified the correct_option to use an enum with
    allowed values: option_a, option_b, option_c, and option_d

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :quiz
 
+  enum :correct_option, {
+    option_a: "option_a",
+    option_b: "option_b",
+    option_c: "option_c",
+    option_d: "option_d",
+  }
+
   def correct_answer_text
     case correct_option
     when "option_a"
