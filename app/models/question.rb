a240c87b5609b42285fb7aec0c5bcbe8d6f5c726
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2025-03-04 09:20:30 +0000

    Modified correct answer lookup for MCQ display
    - Added a 'correct_answer_text' method to the Question model to contain the logic of retrieving the answer text for the correct option
      - This replaces the use of '#send' in the view, which was cryptic and potentially unsafe.
    - Updated the quiz show view to call 'question.correct_answer_text' when displaying the correct answer
      - This change makes the view clearer and easier to understand

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,18 @@
 class Question < ApplicationRecord
   belongs_to :quiz
+
+  def correct_answer_text
+    case correct_option
+    when "option_a"
+      option_a
+    when "option_b"
+      option_b
+    when "option_c"
+      option_c
+    when "option_d"
+      option_d
+    else
+      "No answer defined"
+    end
+  end
 end
